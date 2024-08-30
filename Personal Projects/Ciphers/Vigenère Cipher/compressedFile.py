4fd08fbda3a2e95f638fc9c49dd9dbd4b2673021
Astralis17 <aurora.spark17@gmail.com>
2024-08-30 19:26:55 +0100

    Added compressed version for vigenère

---
+++ Personal Projects/Ciphers/Vigenère Cipher/compressedFile.py
@@ -0,0 +1,35 @@
+import functions
+
+plaintext = ""
+ciphertext = ""
+key = ""
+
+print("Welcome to the Vigenère Cipher Tool")
+run = True
+print("Enter 1 to select Encoding")
+print("Enter 2 to select Decoding")
+while run:
+    mode = int(input("Mode: "))
+    if mode !=1:
+        if mode != 2:
+            print("ERROR")
+        else:
+            run = False
+            print("~~~~~~~~~~~~~~~~~")
+            print("DECODING SELECTED")
+            print("~~~~~~~~~~~~~~~~~")
+    else:
+        run = False
+        print("~~~~~~~~~~~~~~~~~")
+        print("ENCODING SELECTED")
+        print("~~~~~~~~~~~~~~~~~")
+if mode == 1:
+    plaintext = input("Input your plaintext: ")
+    key = input("Key: ")
+    print(functions.encoding(plaintext, functions.keyproccessing(ciphertext, key)))
+else:
+    ciphertext = input("Input your ciphertext: ")
+    key = input("Key: ")
+    print(functions.decoding(ciphertext, functions.keyproccessing(ciphertext, key)))
+
+
